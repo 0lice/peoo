ef5466a1f5a2064df6b553df0b66889285c46361
lice <161068442+0lice@users.noreply.github.com>
2024-10-15 20:30:21 +0000

    ok 2

---
+++ lista 08/01.py
@@ -0,0 +1,11 @@
+a = int(input('Insira o valor de a: '))
+b = int(input('Insira o valor de b: '))
+c = int(input('Insira o valor de c: '))
+
+class Equação:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def
